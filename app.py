7c1f998f61f2948936f63741d485bf031d1a6450
Jonah Werhan <werhanjonah@gmail.com>
2024-01-10 22:20:24 +1000

    Reworked session config to use signed cookies

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import os 
 
-import datetime
+# import datetime
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+import secrets
 
 from functions import login_required, errorPage
 
 # Configure application
 app = Flask(__name__)
 
-# Configure session to use filesystem (instead of signed cookies)
+# Set a secure and random secret key
+session_key = secrets.token_hex(16)
+app.config["SECRET_KEY"] = session_key
+
+# Configure session to use signed cookies (default session type)
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
